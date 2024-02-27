c4ae2e582754a1311a2a54f6d2c0560891d4597d
Kapitosha <artpolevtsov63@mail.ru>
2024-02-27 20:18:59 +0300

    Added file extension validation and error handling related to file crawling

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
+import logging
 import os
 import asyncio
 
-async def recursive_traversal(directory):
+valid_extensions = ["pdf", "doc", "docx"]; #При необходимости добавить
+def read_file(): #Заглушка, здесь должен быть парсер
+    return 0
+
+def form_validation(extension):
+    for i in valid_extensions:
+        if i == extension:
+            return 1
+    return 0
+
+async def recursive_traversal(directory, extension):
+    if not form_validation(extension):
+        print("Invalid file format entered")
+        return 0
     for entry in os.scandir(directory):
-        if entry.is_file() and entry.name.endswith('.pdf'):
-            print(entry.path)  # Выводим путь к файлу
-        elif entry.is_dir():
-            await recursive_traversal(entry.path)  # Рекурсивно обходим директорию асинхронно
+        try:
+            if entry.is_file() and entry.name.endswith('.' + extension):
+                    print(entry.path)  # Выводим путь к файлу
+                    read_file()
+            elif entry.is_dir():
+                await recursive_traversal(entry.path, extension)  # Рекурсивно обходим директорию асинхронно
+        except Exception as e:
+            logging.error('Error: %s') %e.message
+
+
 
 # Пример использования
 async def main():
-    directory_path = 'Z:/Program Files (x86)/Steam'
-    await recursive_traversal(directory_path)
+    directory_path = 'C:/Users/artpo/PycharmProjects/SauronEye'
+    await recursive_traversal(directory_path, 'pdf')
+
 
 asyncio.run(main())
