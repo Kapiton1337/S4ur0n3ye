cb3a3c2a7c1d1aee0a4254f567952192b27d8b05
chichimba <158291303+chichimba@users.noreply.github.com>
2024-02-27 19:12:13 +0300

    Update cvs_parser.py

--- cvs_parser.py
+++ cvs_parser.py
@@ -1,23 +1,18 @@
-import csv
+from file_parser import InformalParserInterface
+import csv 
+import re
 
-def parse_csv(file_path):
-    data = []
+class CsvParser(InformalParserInterface):
+    def check_file(full_file_name: str, target, is_regex: bool, use_ocr: bool) -> bool:
+        with open(full_file_name, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
 
-    with open(file_path, newline='', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            data.append(row)
+            for row in reader:
+                row_text = ','.join(row)
+                
+                if not is_regex and target in row_text:
+                    return True
+                elif is_regex and target.search(row_text):
+                    return True
 
-    return data
-
-def search_keyword(data, keyword):
-    found_rows = [row for row in data if any(keyword in cell for cell in row)]
-    return found_rows
-
-#example
-file_path = 'example.csv'
-keyword = 'example'
-parsed_data = parse_csv(file_path)
-found_rows = search_keyword(parsed_data, keyword)
-for row in found_rows:
-    print(row)
+        return False
