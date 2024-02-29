991748b2bf4fe966ea8b10fac3305961489b1208
Kapitosha <artpolevtsov63@mail.ru>
2024-02-29 18:26:53 +0300

    Added the ability to search by several extensions

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ from odt_parser import OdtParser
 from pdf_parser import PdfParser
 from rtf_parser import RtfParser
 
-valid_extensions = ["pdf", "doc", "docx"]; #При необходимости добавить
-
 extension_to_parser = {
     "pdf": PdfParser,
     "odt": OdtParser,
@@ -25,24 +23,15 @@ async def read_file(parser, file_path, target, is_regex, is_ocr):
         print(file_path)
     return 0
 
-
-def form_validation(extension):
-    for i in valid_extensions:
-        if i == extension:
-            return 1
-    return 0
-
-async def recursive_traversal(tg, directory, extension, target, is_regex, is_ocr):
-        if not form_validation(extension):
-            print("Invalid file format entered")
-            return 0
+async def recursive_traversal(tg, directory, extensions, target, is_regex, is_ocr):
         for entry in os.scandir(directory):
             try:
-                if entry.is_file() and entry.name.endswith('.' + extension): # and extension_to_parser[...]!=None ?
-                        print("proccessing: " + entry.path)  # Выводим путь к файлу
-                        tg.create_task(read_file(extension_to_parser["pdf"], entry.path, target, is_regex, is_ocr)) # change pdf to ext
+                if entry.is_file() and any(entry.name.endswith('.' + ext) for ext in extensions):
+                    print("proccessing: " + entry.path)  # Выводим путь к файлу
+                    ext = entry.name.split('.')[-1]  # Получаем расширение файла
+                    tg.create_task(read_file(extension_to_parser["pdf"], entry.path, target, is_regex, is_ocr)) # change pdf to ext
                 elif entry.is_dir():
-                    await recursive_traversal(tg, entry.path, extension, target, is_regex, is_ocr)  # Рекурсивно обходим директорию асинхронно
+                    await recursive_traversal(tg, entry.path, extensions, target, is_regex, is_ocr)  # Рекурсивно обходим директорию асинхронно
             except Exception as e:
                 logging.error('Error: %s' %e.message) 
 
@@ -52,7 +41,7 @@ async def recursive_traversal(tg, directory, extension, target, is_regex, is_ocr
 async def main():
     async with asyncio.TaskGroup() as tg:
         directory_path = './'
-        await recursive_traversal(tg, directory_path, 'pdf', '1', False, False)
+        await recursive_traversal(tg, directory_path, ["pdf", "txt"], '1', False, False)
 
 
 asyncio.run(main())
