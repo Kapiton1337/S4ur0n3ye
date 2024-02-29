45d1cca615cf91b1c1a735dd0073ee2459538393
maksim <maxmarinenkov68@gmail.com>
2024-02-29 19:19:40 +0300

    Add ocr parser

---
+++ ocr_parser.py
@@ -0,0 +1,29 @@
+import easyocr
+
+
+class OCRParser:
+    reader = None
+
+    def __init__(self, lang_list=None):
+        if lang_list is None:
+            lang_list = ['ru', 'en']
+        self.reader = easyocr.Reader(lang_list=lang_list,
+                                     gpu=False,
+                                     detect_network="craft",
+                                     recog_network='standard',
+                                     download_enabled=False,
+                                     detector=True,
+                                     recognizer=True,
+                                     verbose=True,
+                                     quantize=True,
+                                     cudnn_benchmark=False)
+
+    def pdf_ocr(self, img, target, is_regex: bool) -> bool:
+        target = target.lower()
+        text = self.reader.readtext(img, detail=0, paragraph=True)
+        text = "".join(text).lower()
+        if not is_regex and target in text:
+            return True
+        if is_regex and target.search(text) is not None:
+            return True
+        return False
--- pdf_parser.py
+++ pdf_parser.py
@@ -1,4 +1,4 @@
-import easyocr #pip install easyocr
+import easyocr  # pip install easyocr
 
 from file_parser import InformalParserInterface
 import fitz  # install using: pip install PyMuPDF
@@ -6,7 +6,7 @@ import fitz  # install using: pip install PyMuPDF
 
 class PdfParser(InformalParserInterface):
     def check_file(full_file_name: str, target, is_regex: bool,
-                   use_ocr: bool) -> bool:  # Target should be str or re.compile() result
+                   ocr=None) -> bool:  # Target should be str or re.compile() result
         with fitz.open(full_file_name) as doc:
             for page in doc:
                 text = page.get_text()
@@ -14,24 +14,11 @@ class PdfParser(InformalParserInterface):
                     return True
                 if is_regex and target.search(text) is not None:
                     return True
-                if not use_ocr:
+                if ocr is None:
                     continue
                 imgs = page.get_images()
                 for img in imgs:
-                    if pdf_ocr(doc.extract_image(img[0])['image'], target, is_regex):
+                    if ocr.pdf_ocr(doc.extract_image(img[0])['image'], target, is_regex):
                         return True
         return False
 
-
-def pdf_ocr(img: dict, target, is_regex: bool) -> bool:
-    reader = easyocr.Reader(lang_list=['ru', 'en'], gpu=False, model_storage_directory=None,
-                            user_network_directory=None, detect_network="craft",
-                            recog_network='standard', download_enabled=False,
-                            detector=True, recognizer=True, verbose=True,
-                            quantize=True, cudnn_benchmark=False)
-    text = reader.readtext(img, detail=0)
-    if not is_regex and target in text:
-        return True
-    if is_regex and target.search(text) is not None:
-        return True
-    return False
