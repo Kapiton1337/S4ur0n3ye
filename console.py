7bd177aa6fd9a1c2e01d8e1c5930aa85325a2ee6
maksim <maxmarinenkov68@gmail.com>
2024-02-29 22:00:31 +0300

    bug fix

--- console.py
+++ console.py
@@ -1,4 +1,3 @@
-
 from argparse import ArgumentParser, Namespace
 import logging
 import os
@@ -8,34 +7,32 @@ import re
 
 from cvs_parser import CsvParser
 from html_parser import HtmlParser
+from ocr_parser import OCRParser
 from odt_parser import OdtParser
 from pdf_parser import PdfParser
 from rtf_parser import RtfParser
 
 parser = ArgumentParser(usage='\r      \n=== SauronEye for PDF ===\n\nUsage: %(prog)s [OPTIONS]+ argument')
 
-parser.add_argument('-t', dest="target", metavar="target" ,  help='Word or phrase to search for')
+parser.add_argument('-t', dest="target", metavar="target", help='Word or phrase to search for')
 parser.add_argument('--filetypes', default='pdf', help=' Filetypes to search for')
-parser.add_argument('-r', '--regex',action='store_true', help='use this flag if "target" is a regular expression' )
-parser.add_argument("--fullpath",action='store_true', help="Displays the full path") 
+parser.add_argument('-r', '--regex', action='store_true', help='use this flag if "target" is a regular expression')
+parser.add_argument("--fullpath", action='store_true', help="Displays the full path")
 parser.add_argument('-d', '--directories', nargs='*', metavar='directory', help="Directories to search")
 parser.add_argument('-f', '--files', dest="files", nargs='*', help="path to file/files")
-parser.add_argument('--use_ocr',action='store_true', help='Use the OCR method to search for text in pdf images' )
+parser.add_argument('--use_ocr', action='store_true', help='Use the OCR method to search for text in pdf images')
 
-args: Namespace= parser.parse_args()
-
-
+args: Namespace = parser.parse_args()
 
 
-
-def argcheck(): # модуль в разработке 
+def argcheck():  # модуль в разработке
     print(args)
     if (not args.target and not args.regex) or (not args.directories and not args.onefile):
         print("Error: No word and/or directory\n")
         parser.print_help()
         exit()
 
-    
+
 extension_to_parser = {
     "pdf": PdfParser,
     "odt": OdtParser,
@@ -45,32 +42,38 @@ extension_to_parser = {
 }
 
 
-
-async def read_file(parser, file_path, target, is_regex, is_ocr): 
+async def read_file(parser, file_path, target, is_regex, is_ocr):
     if parser.check_file(file_path, target, is_regex, is_ocr):
         print(file_path)
     return 0
 
+
 async def recursive_traversal(tg, directory, extensions, target, is_regex, is_ocr):
-        for entry in os.scandir(directory):
-            try:
-                if entry.is_file() and any(entry.name.endswith('.' + ext) for ext in extensions):
-                    print("proccessing: " + entry.path)  # Выводим путь к файлу
-                    ext = entry.name.split('.')[-1]  # Получаем расширение файла
-                    tg.create_task(read_file(extension_to_parser[ext], entry.path, target, is_regex, is_ocr)) # change pdf to ext
-                elif entry.is_dir():
-                    await recursive_traversal(tg, entry.path, extensions, target, is_regex, is_ocr)  # Рекурсивно обходим директорию асинхронно
-            except Exception as e:
-                logging.error('Error: %s' %e.message) 
+    for entry in os.scandir(directory):
+        try:
+            if entry.is_file() and any(entry.name.endswith('.' + ext) for ext in extensions):
+                print("proccessing: " + entry.path)  # Выводим путь к файлу
+                ext = entry.name.split('.')[-1]  # Получаем расширение файла
+                tg.create_task(
+                    read_file(extension_to_parser[ext], entry.path, target, is_regex, is_ocr))  # change pdf to ext
+            elif entry.is_dir():
+                await recursive_traversal(tg, entry.path, extensions, target, is_regex,
+                                          is_ocr)  # Рекурсивно обходим директорию асинхронно
+        except Exception as e:
+            logging.error('Error: %s' % e.message)
+
+        # Пример использования
 
 
-
-# Пример использования
 async def main():
     if args.regex:
-        main_target=re.compile(args.target)
+        main_target = re.compile(args.target)
     else:
-        main_target=args.target
+        main_target = args.target
+    if args.regex:
+        ocr = OCRParser()
+    else:
+        ocr = None
     async with asyncio.TaskGroup() as tg:
         if args.directories:
             for directory_path in args.directories:
@@ -78,8 +81,7 @@ async def main():
         elif files:
             for file in files:
                 ext = file.split('.')[-1]  # Получаем расширение файла
-                read_file(extension_to_parser[ext], file, target, is_regex, is_ocr)
-
+                read_file(extension_to_parser[ext], file, target, is_regex, ocr)
 
 
 asyncio.run(main())
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import asyncio
 import logging
 import os
-import asyncio
-from ocr_parser import OCRParser
 
 from cvs_parser import CsvParser
 from html_parser import HtmlParser
@@ -19,29 +18,28 @@ extension_to_parser = {
 
 
 async def read_file(parser, file_path, target, is_regex, is_ocr):
-    if is_ocr:
-        ocr = OCRParser()
-    else:
-        ocr = None
-    if parser.check_file(file_path, target, is_regex, ocr):
+    if parser.check_file(file_path, target, is_regex, is_ocr):
         print(file_path)
     return 0
 
+
 async def recursive_traversal(tg, directory, extensions, target, is_regex, is_ocr):
-        for entry in os.scandir(directory):
-            try:
-                if entry.is_file() and any(entry.name.endswith('.' + ext) for ext in extensions):
-                    print("proccessing: " + entry.path)  # Выводим путь к файлу
-                    ext = entry.name.split('.')[-1]  # Получаем расширение файла
-                    tg.create_task(read_file(extension_to_parser["pdf"], entry.path, target, is_regex, is_ocr)) # change pdf to ext
-                elif entry.is_dir():
-                    await recursive_traversal(tg, entry.path, extensions, target, is_regex, is_ocr)  # Рекурсивно обходим директорию асинхронно
-            except Exception as e:
-                logging.error('Error: %s' %e.message) 
+    for entry in os.scandir(directory):
+        try:
+            if entry.is_file() and any(entry.name.endswith('.' + ext) for ext in extensions):
+                print("proccessing: " + entry.path)  # Выводим путь к файлу
+                ext = entry.name.split('.')[-1]  # Получаем расширение файла
+                tg.create_task(
+                    read_file(extension_to_parser["pdf"], entry.path, target, is_regex, is_ocr))  # change pdf to ext
+            elif entry.is_dir():
+                await recursive_traversal(tg, entry.path, extensions, target, is_regex,
+                                          is_ocr)  # Рекурсивно обходим директорию асинхронно
+        except Exception as e:
+            logging.error('Error: %s' % e.message)
+
+        # Пример использования
 
 
-
-# Пример использования
 async def main():
     async with asyncio.TaskGroup() as tg:
         directory_path = './'
