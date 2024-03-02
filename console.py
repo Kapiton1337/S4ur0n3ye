cf2ed1c26c34c9eceaa5e45ae342b1c7ba6ebd30
DrewNoCap <101293320+DrewNoCap@users.noreply.github.com>
2024-03-03 02:05:55 +0300

    Update console.py

--- console.py
+++ console.py
@@ -44,7 +44,7 @@ args: Namespace = parser.parse_args()
 
 
 def argcheck():  
-    if (not args.target and not args.regex) or (not args.directories and not args.onefile):
+    if (not args.target) or (not args.directories and not args.onefile):
         print("Error: No target and/or directory\n")
         parser.print_help()
         exit()
