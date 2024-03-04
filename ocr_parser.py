6956b4038c1a789bda40822577c3b71520219d21
maxkazak <max-kazak@ya.ru>
2024-03-04 21:34:37 +0300

    fix

--- ocr_parser.py
+++ ocr_parser.py
@@ -8,20 +8,20 @@ class OCRParser:
         if lang_list is None:
             lang_list = ['ru', 'en']
         self.reader = easyocr.Reader(lang_list=lang_list,
-                                     gpu=False,
+                                     gpu=True,
                                      detect_network="craft",
                                      recog_network='standard',
                                      download_enabled=True,
                                      detector=True,
                                      recognizer=True,
-                                     verbose=False,
+                                     verbose=True,
                                      quantize=True,
                                      cudnn_benchmark=False)
 
     def pdf_ocr(self, img, target, is_regex: bool) -> bool:
         text = self.reader.readtext(img, detail=0, paragraph=True)
         text = "".join(text).lower()
-        if not is_regex and target in text:
+        if not is_regex and target.lower() in text:
             return True
         if is_regex and target.search(text) is not None:
             return True
--- rtf_parser.py
+++ rtf_parser.py
@@ -3,12 +3,11 @@ from file_parser import InformalParserInterface
 
 class RtfParser(InformalParserInterface):
     def check_file(full_file_name: str, target, is_regex: bool, ocr) -> bool:
-        target = target.lower()
         with open(full_file_name, 'r') as file:
             rtf_content = file.read().lower()
 
         
-        if not is_regex and target in rtf_content:
+        if not is_regex and target.lower() in rtf_content:
             return True
         if is_regex and target.search(rtf_content) != None:
             return True
